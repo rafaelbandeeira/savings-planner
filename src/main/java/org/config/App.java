d819677465ed9092ef7c31b801f4eb26e9092a63
Rafael Bandeira <rafael.bandeira@live.com>
2023-04-02 01:45:20 -0300

    created abstract class with constants and a method to get the correct environment

--- src/main/java/org/config/App.java
+++ src/main/java/org/config/App.java
@@ -2,11 +2,11 @@ package org.config;
 
 import java.util.Objects;
 
-public abstract class Application {
+public abstract class App {
     public static final String DEV_SAVING_PLANNER = "http://qa-assignment.useorigin.com.s3-website-us-east-1.amazonaws.com/";
     public static final String PROD_SAVING_PLANNER = "";
 
-    public static String getEnvApp() {
+    public static String getAppUrl() {
         if (Objects.equals(System.getenv("ENV"), "PROD"))
             return PROD_SAVING_PLANNER;
         return DEV_SAVING_PLANNER;
