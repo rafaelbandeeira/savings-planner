5dc6aba788f9025cb0ccf3baae7b664ecaff7497
Rafael Bandeira <rafael.bandeira@live.com>
2023-04-02 01:47:44 -0300

    added savings plan page elements and coded into fluent page pattern

--- src/main/java/org/pages/SavingsPage.java
+++ src/main/java/org/pages/SavingsPage.java
@@ -1,2 +1,63 @@
-package org.pages;public class SavingsPage {
+package org.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import javax.xml.xpath.XPath;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class SavingsPage {
+    private WebDriver driver;
+    @FindBy(css = "[data-testid=\"input\"]")
+    private WebElement amountField;
+    @FindBy(css = "[data-testid=\"reachDateIncrement\"]")
+    private WebElement incrementDate;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div/div[3]/div/div[1]/div/div[2]/p/text()[2]")
+    private WebElement monthlyDeposit;
+
+    private static String totalAmount;
+    private static int months;
+
+    public SavingsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public SavingsPage insertAmount(String totalAmount) {
+        this.totalAmount = totalAmount;
+        amountField.sendKeys(totalAmount);
+
+        return new SavingsPage(driver);
+    }
+
+    private BigDecimal getTotalAmount() {
+        String intNumber = totalAmount.substring(0, totalAmount.length() -2);
+        String decNumber = totalAmount.substring(totalAmount.length() - 2);
+        String fullNumber = intNumber + "." + decNumber;
+
+        return new BigDecimal(fullNumber);
+    }
+
+    public SavingsPage setEndDate(int months) {
+        this.months = months;
+        int count = 0;
+        while (count < months) {
+            incrementDate.click();
+            count++;
+        }
+
+        return new SavingsPage(driver);
+    }
+
+    public BigDecimal getMonthlyDeposit() {
+        String value = monthlyDeposit.getText().replace(Character.toString(','), "");
+        return new BigDecimal(value);
+    }
+
+    public BigDecimal divideByMonth() {
+        return getTotalAmount().divide(new BigDecimal(months), 2, RoundingMode.HALF_UP);
+    }
 }
