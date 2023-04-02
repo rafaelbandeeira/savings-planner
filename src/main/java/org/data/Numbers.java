3a64f1b24577aab877769d93225befd14e49d604
Rafael Bandeira <rafael.bandeira@live.com>
2023-04-02 16:18:46 -0300

    created class with utility methods for data generation

--- src/main/java/org/data/Numbers.java
+++ src/main/java/org/data/Numbers.java
@@ -1,2 +1,10 @@
-package org.data;public class Numbers {
+package org.data;
+
+import java.util.Random;
+
+public class Numbers {
+    public static String getRandomInt(int min, int max) {
+        Random random = new Random();
+        return String.valueOf(random.nextInt(max - min) + min);
+    }
 }
